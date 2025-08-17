0dd11cc3cc50a33a88f231662ba98e60309e511e
dmtrhdev <hrytsak.dev@gmail.com>
2025-08-17 22:02:26 +0300

    Add browser headers to Djinni scraper

--- scraper.go
+++ scraper.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 	"strings"
+	"time"
 
 	"golang.org/x/net/html"
 )
@@ -71,13 +73,28 @@ func NewDjinniScraper() *DjinniScraper {
 }
 
 func (s *DjinniScraper) Jobs() ([]Job, error) {
-	req, err := http.NewRequest("GET", "https://djinni.co/jobs/?primary_keyword=JavaScript&primary_keyword=Angular&primary_keyword=React.js&primary_keyword=Svelte&primary_keyword=Vue.js&primary_keyword=Markup", nil)
+	base := "https://djinni.co/jobs/"
+	qs := url.Values{}
+	qs.Add("primary_keyword", "JavaScript")
+	qs.Add("primary_keyword", "Angular")
+	qs.Add("primary_keyword", "React.js")
+	qs.Add("primary_keyword", "Svelte")
+	qs.Add("primary_keyword", "Vue.js")
+	qs.Add("primary_keyword", "Markup")
+	qs.Add("_", fmt.Sprint(time.Now().Unix()))
+
+	req, err := http.NewRequest("GET", base+"?"+qs.Encode(), nil)
 	if err != nil {
 		return nil, fmt.Errorf("djinni: %w", err)
 	}
-
 	// Djinni shows old jobs if you don't look like a browser.
-	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "+
+		"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+	req.Header.Set("Accept-Language", "uk-UA,uk;q=0.9,en-US;q=0.8,en;q=0.7")
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("Pragma", "no-cache")
+	req.Header.Set("Referer", "https://djinni.co/jobs/")
 
 	resp, err := s.client.Do(req)
 	if err != nil {
